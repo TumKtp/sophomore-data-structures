a6fc5ee964a820cbd7edf8dab8cdf1ffef6ff304
TumKtp <tumktp.dev@gmail.com>
2022-09-28 21:31:37 +0700

    Optimize erase many

--- d58_q1_erase_many/student.h
+++ d58_q1_erase_many/student.h
@@ -1,29 +1,31 @@
 #ifndef __STUDENT_H_
 #define __STUDENT_H_
-
+#include <bits/stdc++.h>
+using namespace std;
 template <typename T>
 void CP::vector<T>::erase_many(const std::vector<int> &pos)
 {
   // write your code here
   T *arr = new T[mSize - pos.size()]();
   int idx = 0, id = 0;
+  std::set<int> temp;
+  for (int i = 0; i < pos.size(); i++)
+    temp.insert(pos[i]);
+
   for (int i = 0; i < mSize; i++)
   {
-    // std::cout << i << pos[idx] << std::endl;
-    if (i != pos[idx])
+    bool chk = false;
+    auto it = temp.find(i);
+
+    if (it == temp.end() || *it != i)
     {
       arr[id++] = mData[i];
     }
-    else
-    {
-      idx++;
-    }
   }
+  delete[] mData;
   mSize = mSize - pos.size();
-  for (int i = 0; i < mSize; i++)
-  {
-    mData[i] = arr[i];
-  }
+  mCap = mSize;
+  mData = arr;
 }
 
 #endif
