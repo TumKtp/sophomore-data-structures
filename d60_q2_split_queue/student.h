08dff315c1b95ebe5bacb70df57a0db4ef7ac40e
TumKtp <tumktp.dev@gmail.com>
2022-09-28 19:53:05 +0700

    Finish d60_q2_split_queue

--- d60_q2_split_queue/student.h
+++ d60_q2_split_queue/student.h
@@ -9,18 +9,19 @@
 #include "queue.h"
 
 template <typename T>
-std::vector< CP::queue<T> > CP::queue<T>::split_queue(int k) {
-    std::vector<CP::queue<T> > qs(k);
-    // code here
-    //
-    //
-    //
-    //
-    //
-    //
-    //
+std::vector<CP::queue<T>> CP::queue<T>::split_queue(int k)
+{
+    std::vector<CP::queue<T>> qs(k);
+    int i = 0;
+    while (!empty())
+    {
+        T top = this->front();
+        this->pop();
+        qs[i % k].push(top);
+        i++;
+    }
+
     return qs;
 }
 
 #endif
-
